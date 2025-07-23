2c38082b4267b2d3f795f0d82ae29acd1b9f116f
waleedelswerky12-png <waleedelswerky12@gmail.com>
2025-07-23 14:50:07 +0400

    Add bloza-clone/.next/static/chunks/src_app_layout_tsx_68b267f5._.js 1753267807083

---
+++ bloza-clone/.next/static/chunks/src_app_layout_tsx_68b267f5._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__404369a3._.css",
+  "static/chunks/_b841f90f._.js"
+],
+    source: "dynamic"
+});
